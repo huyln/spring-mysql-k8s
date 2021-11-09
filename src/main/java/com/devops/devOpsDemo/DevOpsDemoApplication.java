8e241a8860e1c1ee4cfb74ff6cc648b47c977645
Huy3 Nguyen Quang <nqhuy3@IfiSolution.local>
2021-11-09 10:43:29 +0700

    initial

---
+++ src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.devops.devOpsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is the new change-testing webhook-- final");
+		SpringApplication.run(DevOpsDemoApplication.class, args);
+	}
+
+}
